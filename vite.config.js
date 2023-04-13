77ad19a55ce241f159f1aa36f00ec13571d46ec3
City of Vernonia GIS <gis@vernonia-or.gov>
2023-04-13 07:21:15 -0700

    move all custom vernonia scss to map application

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 // https://vitejs.dev/config/
+import { ViteEjsPlugin } from 'vite-plugin-ejs';
+import _package from './package.json';
+
+const calciteVersion = _package.dependencies['@esri/calcite-components'].replace('^', '').replace('~', '');
+const calciteUrl = `https://js.arcgis.com/calcite-components/${calciteVersion}/calcite.esm.js`;
 
 export default {
+  plugins: [
+    ViteEjsPlugin({
+      calciteUrl,
+    }),
+  ],
   root: './dev',
   server: {
     port: 8080,
